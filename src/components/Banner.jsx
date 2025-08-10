0c989d91a44ba914c0cf1369b8ac5d644f4c6a82
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-08-11 04:41:27 +0600

    added animation on background star

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,16 +6,16 @@ import Image from "next/image";
 import SmarterIdia from "@/shared/SmarterIdia";
 import { motion } from "motion/react";
 const Banner = () => {
-
   return (
-    <div className="pt-[111px] md:pb-[100px] pb-[50px] relative bg-[url('/assets/BannerImage.png')] bg-no-repeat bg-cover">
-      <div className="w-11/12 mx-auto">
+    <div className="pt-[111px] md:pb-[100px] pb-[50px] relative bg-[url('/assets/BannerImg.png')] bg-no-repeat bg-cover z-40">
+      <div className="w-11/12 mx-auto z-20">
         <SmarterIdia></SmarterIdia>
         <motion.div
-        initial={{ opacity: 0, y: 24, scale: 0.9 }}
-        animate={{ opacity: 1, y: 0, scale: 1 }}
-        transition={{ duration: 1 }}
-        className="text-center text-[32px] md:text-[120px] -space-y-10 bg-gradient-to-b from-[#00FFE0] to-white bg-clip-text text-transparent border-white mt-[30px] md:mt-0">
+          initial={{ opacity: 0, y: 24, scale: 0.9 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="text-center text-[32px] md:text-[120px] -space-y-10 bg-gradient-to-b from-[#00FFE0] to-white bg-clip-text text-transparent border-white mt-[30px] md:mt-0"
+        >
           <p>We are your Software</p>
           <p className="flex justify-center items-center gap-5 h-[140px]">
             Development{" "}
@@ -40,57 +40,15 @@ const Banner = () => {
         </div>
       </div>
       <Service></Service>
-      {/* <div>
-        <Image
-          src="/assets/leftsideImg.png"
-          alt="banner star"
-          width={500}
-          height={500}
-          className="md:w-[600px] w-[250px] h-auto absolute -top-45 left-0"
-        />
-        <Image
-          src="/assets/rightsideImg.png"
-          alt="banner star"
-          width={500}
-          height={500}
-          className="md:w-[600px] w-[250px] h-auto absolute -top-45 right-0"
-        />
-        <Image
-          src="/assets/bgstar.png"
-          alt="banner star"
-          width={300}
-          height={300}
-          className="w-[300px] h-auto absolute top-0 left-[60%] hidden md:block animate-pulse"
-        />
-        <Image
-          src="/assets/bgstar.png"
-          alt="banner star"
-          width={300}
-          height={300}
-          className="w-[300px] h-auto absolute top-[40%] right-20 hidden md:block animate-pulse"
-        />
-        <Image
-          src="/assets/bgstar2.png"
-          alt="banner star"
-          width={200}
-          height={200}
-          className="w-[120px] h-auto absolute top-[50%] left-20 hidden md:block animate-pulse"
-        />
-        <Image
-          src="/assets/bgstar2.png"
-          alt="banner star"
-          width={300}
-          height={300}
-          className="w-[100px] h-auto absolute top-[20%] left-[20%] animate-pulse"
-        />
-        <Image
-          src="/assets/bgstar.png"
-          alt="banner star"
-          width={300}
-          height={300}
-          className="w-[300px] h-auto absolute md:-top-10 md:left-[20%] left-0 top-0 animate-pulse"
-        />
-      </div> */}
+      <Image
+        src="/assets/BannerStar.png"
+        alt="banner"
+        width={500}
+        height={500}
+        placeholder="blur"
+        blurDataURL="/assets/BannerStar.png"
+        className="w-[90vw] md:h-[90vh] h-screen absolute top-20 left-0 -z-10 animate-pulse "
+      />
     </div>
   );
 };
